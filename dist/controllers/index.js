12534e9c734cb88d2c3e078114dd2f47179ade44
Alex Grigore <alex.grigore@hogarthww.com>
2020-03-18 19:30:24 +0200

    ignore dist

--- dist/controllers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Image_1 = require("./Image/Image");
-var Report_1 = require("./Report/Report");
-var imageController = new Image_1.ImageController();
-exports.imageController = imageController;
-var reportController = new Report_1.ReportController();
-exports.reportController = reportController;
--- dist/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Image_1 = require("./Image/Image");
-exports.imageRouter = Image_1.router;
-var Report_1 = require("./Report/Report");
-exports.reportRouter = Report_1.router;
